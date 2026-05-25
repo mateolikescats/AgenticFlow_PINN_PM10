99f17833e086b7d93a90cf697586bf5d389e3c84
Camilo Andrés Espejo Marín <cespejom@unal.edu.co>
2026-05-24 23:20:50 -0500

    No sé qué hace esa carpeta clonada allí jeje, la borré para trabajar más limpiamente

--- AgenticFlow_PINN_PM10-main/AgenticFlow_PINN_PM10/test_compile.jl
+++
@@ -1,5 +0,0 @@
-using Pkg
-Pkg.add("DomainSets")
-include("src/pinn/AdvectionDiffusion.jl")
-using .AdvectionDiffusion
-println("SUCCESS")
--- AgenticFlow_PINN_PM10-main/test_compile.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-Pkg.activate(".")
-println("Instantiating project environment (this might download packages)...")
-Pkg.instantiate()
-println("Including AdvectionDiffusion.jl...")
-include("src/pinn/AdvectionDiffusion.jl")
-using .AdvectionDiffusion
-println("SUCCESS")
